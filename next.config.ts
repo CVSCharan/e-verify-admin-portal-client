96703186ff0c09d157b281f25bafd7f2ec137abf
CVS CHARAN <cvs@CVSs-MacBook-Pro.local>
2025-03-13 07:19:33 +0530

    initial setup of migration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["github.com", "raw.githubusercontent.com"],
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    // Add other environment variables here as needed
+  },
 };
 
 export default nextConfig;
